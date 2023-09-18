069c954d7d11adc45edf30d0b09e1fef82978a2b
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-09-18 19:34:08 +0200

    fix: middy

--- functions/PostLogin/index.js
+++ functions/PostLogin/index.js
@@ -2,6 +2,7 @@ const { sendResponse, sendError } = require("../../responses/index");
 const { db } = require("../../services/db");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const middy = require("@middy/core");
 const { validateToken } = require("../../middleware/auth");
 
 async function getUserByEmail(email) {
@@ -15,31 +16,33 @@ async function getUserByEmail(email) {
   return result.Items[0];
 }
 
-exports.handler = async (event) => {
-  try {
-    const { email, password } = JSON.parse(event.body);
+const handler = middy()
+  .handler(async (event) => {
+    try {
+      if (event?.error && event?.error === "401") {
+        return sendError(401, "Invalid token");
+      }
 
-    const user = await getUserByEmail(email);
+      const { email, password } = JSON.parse(event.body);
 
-    if (!user) {
-      return sendError(404, "User not found");
+      const user = await getUserByEmail(email);
+
+      if (!user) {
+        return sendError(404, "User not found");
+      }
+
+      const validPassword = await bcrypt.compare(password, user.password);
+
+      if (!validPassword) {
+        return sendError(401, "Invalid password");
+      }
+
+      const token = jwt.sign({ userId: user.id }, "a1b1c1");
+      return sendResponse(200, { token });
+    } catch (error) {
+      return sendError(500, error.message);
     }
+  })
+  .use(validateToken);
 
-    const validPassword = await bcrypt.compare(password, user.password);
-
-    if (!validPassword) {
-      return sendError(401, "Invalid password");
-    }
-
-    const token = jwt.sign({ userId: user.id }, "a1b1c1");
-
-    return sendResponse(200, { token });
-  } catch (error) {
-    await validateToken.onError({ event, response: {} });
-
-    if (event.error) {
-      return sendError(401, "Unauthorized");
-    }
-    return sendError(500, error.message);
-  }
-};
+module.exports = { handler };
